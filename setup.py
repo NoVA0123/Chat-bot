75e03793cd2a81f41a672580bb485e7f6c1ed8fa
Abhijit Rai <abhijit0123rai@gmail.com>
2024-03-27 14:24:11 +0530

    created simple chatbot package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+# creating a setup for our model
+
+setup(name='Simple Chatbot',
+      version='0.0.0',
+      author="Abhijit 'N0v4' Rai",
+      author_email='abhijit0123rai@yahoo.com',
+      packages=find_packages(),
+      install_requires=[])
